b9fb2b8de758775e67520d9ba19bf5a4b0c34ecf
bafc13 <art.bryzghalin@mail.ru>
2025-05-11 19:23:37 +0300

    initial commit

---
+++ src/javaphone/JavaPhone.java
@@ -0,0 +1,22 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaphone;
+
+/**
+ *
+ * @author bafc13
+ */
+public class JavaPhone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        mainJFrame frame = new mainJFrame();
+//        frame.setVisible(true);
+    }
+
+}
